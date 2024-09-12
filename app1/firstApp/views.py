0987003600c0f2a6c9591c31cc8910552aa0a441
Flavio Arzola <flavioarzolamontecino@gmail.com>
2024-09-12 18:18:04 -0300

    app1 finalizada

--- app1/firstApp/views.py
+++ app1/firstApp/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+def display(request):
+    return HttpResponse("<h1>Hola mundo django prueba 2</h1>")
 
 # Create your views here.
